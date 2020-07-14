637a779d3c3fb05b3073baf870f8060bad5642f2
subhiashraf912 <59281574+subhiashraf912@users.noreply.github.com>
2020-07-15 01:23:17 +0300

    Help

--- src/components/QuizQuestions.jsx
+++ src/components/QuizQuestions.jsx
@@ -11,7 +11,18 @@ import * as firebase from 'firebase';
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import {useHistory} from 'react-router-dom'
+
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+
+
 const useStyles = makeStyles({
+  MainButtons: {
+    margin:'20px'
+  },
   MainContainer: {
     margin: '20px',
     display: 'flex',
@@ -23,6 +34,13 @@ const useStyles = makeStyles({
   },
   media: {
     height: 140,
+  },
+  RedTextWarning: {
+    alignItems: "center",
+    flex: 1,
+    color: "red",
+    justifyContent: "center",
+    textAlign: "center",
   },
 });
 
@@ -37,10 +55,16 @@ export default function QuizQuestions() {
   };
 
 
-  let quizesFromDB = []
   const [ loading, setLoading ] = useState(true)
   const [ DataFromDB, setDataFromDB ] = useState([])
   const [ currentQuestion, setCurrentQuestion ] = useState(0)
+  const [ errorMsg , setErrorMsg ] = useState('')
+  const [ radioValue, setRadioValue ] = useState('')
+const [answers,setAnswers] = useState([])
+  const handleChangeRadio = (elemt) => {
+    setRadioValue(elemt.target.value)
+ }
+
 
   useEffect(() => {
 
@@ -60,9 +84,32 @@ export default function QuizQuestions() {
   }, []);
 
 
+  useEffect(() => {
+  console.log(answers)
+},[answers])
+
+  const handlePressTheButton = () => {
+    setErrorMsg('')
+    setRadioValue('')
+    if (radioValue === "") return setErrorMsg("Please sellect an answer!!!")
+    if (currentQuestion - 1 < DataFromDB.length) {
+      console.log(radioValue)
+      let AnswerObject = {}
+      AnswerObject[ currentQuestion ] = radioValue
+      if (!(currentQuestion) == DataFromDB.length) {
+        console.log(`in the sh!t`)
+        setCurrentQuestion(currentQuestion + 1)
+
+        setAnswers(answers.concat(AnswerObject))
+        return
+      }
 
 
 
+    }
+    else {
+console.log(answers)}
+}
 
   if (loading) return <h1>Still Loading</h1>
 
@@ -74,6 +121,7 @@ export default function QuizQuestions() {
     <Container>
       <h1>Question number {currentQuestion + 1}</h1>
       <h1>Question Title: {DataFromDB[ currentQuestion ].questionTitle}</h1>
+      <h3>Question Description:</h3><p>{DataFromDB[ currentQuestion ].question}</p>
       {/* this is a button that changes the current question (in the state)
       and if the user keeps clicking that button it forces the host to 
       display something from the list that is not exist
@@ -85,12 +133,32 @@ export default function QuizQuestions() {
       
       
       */}
-      <Button onClick={() => {
-        if (currentQuestion < DataFromDB.length - 1){
-          setCurrentQuestion(currentQuestion + 1)
-        }
-      }}>Change number</Button>
-        
+    
+      <br />
+      <h2>Choose the correct answer</h2>
+
+      
+
+      <RadioGroup aria-label="Correct Answer" name="CorrectAns" value={radioValue} onChange={handleChangeRadio}>
+        <FormControlLabel value='1' control={<Radio />} label={DataFromDB[currentQuestion].firstOption} />
+        <FormControlLabel value='2' control={<Radio />} label={DataFromDB[ currentQuestion ].secondOption}  />
+        <FormControlLabel value='3' control={<Radio />} label={DataFromDB[ currentQuestion ].thirdOption}  />
+        <FormControlLabel value='4' control={<Radio />} label={DataFromDB[ currentQuestion ].forthOption}  />
+      </RadioGroup> 
+
+      
+
+
+      
+      <h3 className={classes.RedTextWarning}>{errorMsg}</h3>
+      <Button className="MainButtons" color='primary' variant="contained" onClick={handlePressTheButton}>Next question</Button>
+      
+      {/* 
+      <Button className={classes.MainButtons}variant="contained" color = 'primary'>{DataFromDB[ currentQuestion ].firstOption}</Button>
+      <Button className={classes.MainButtons} variant="contained" color='primary'>{DataFromDB[ currentQuestion ].secondOption}</Button>
+      <Button className={classes.MainButtons} variant="contained" color='primary'>{DataFromDB[ currentQuestion ].thirdOption}</Button>
+      <Button className={classes.MainButtons} variant="contained" color='primary'>{DataFromDB[ currentQuestion ].forthOption}</Button> */}
+
      
     </Container>
   );
